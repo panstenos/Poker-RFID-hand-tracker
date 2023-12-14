6f59914411f8d9d401d6ea3f1b3d3337e72b4595
panstenos <pannstenos@gmail.com>
2023-12-14 16:45:45 +0000

    Version 0.2

--- read.py
+++ read.py
@@ -1,12 +1,16 @@
+import time
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
 try:
-    id, text= reader.read()
-    print(id)
-    print(text)
+    while True:
+        print('Place card on Reader')
+        id, text = reader.read()
+        print('ID: {}'.format(id))
+        print('Text: {}'.format(text))
 
-finally:
+except: # use Ctr+C to terminate the programm
     GPIO.cleanup()
+    print('GPIO Good to Go')
